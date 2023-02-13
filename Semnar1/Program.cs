96048d9f6d43803de93863e19b920357ae8d382e
Elena <shigonoke@mail.ru>
2023-02-13 14:44:31 +0300

    First seminar ended

---
+++ Semnar1/Program.cs
@@ -0,0 +1,57 @@
+﻿// double bigNum = 12.13;
+// bool isEven = true;
+// string word = "flower";
+// char symbol = '$';
+
+// int num1 = -12;
+// int num2 =6;
+// Console.Write("My numbers are " + num1 + " and " +num2 + " and it is good!");
+// Console.WriteLine($"My numbers are {num1} and {num2} and it is good!");
+
+// Console.Write("Input an integer number: ");
+//  int num = Console.ReadLine();
+// SEminar1
+// HomeWork1
+
+// Task1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго
+/*Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int guad2 = num2 * num2;
+
+if(num1 == guad2)
+{
+Console.Write("Yes!");
+}
+else
+{
+Console.Write("No!");    
+}
+*/
+// Task2. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+/*Console.Write("Input a positive number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = -num;
+
+while(current <= num)
+{
+  Console.Write(current + " "); 
+  current++; 
+}
+ */
+
+//  31542 / 10 = 3154
+//  31542 / 10 = 315
+//  31542 / 1000 = 31
+//  31542 / 10000 = 3
+
+//  31542 % 10 = 2
+//  31542 % 100 = 42
+//  31542 % 1000 = 542
+//  31542 % 10000 = 1542
+
+//  453216 -> 32
