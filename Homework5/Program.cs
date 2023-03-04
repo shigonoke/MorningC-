81505f75b3c931dbaa07ee24eb1c9bf006257a10
Elena <shigonoke@mail.ru>
2023-03-04 18:24:52 +0300

    Homework5

--- Homework5/Program.cs
+++ Homework5/Program.cs
@@ -2,37 +2,37 @@
 // Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
 // которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
-Console.WriteLine("Введите размер массива");
-int size = Convert.ToInt32(Console.ReadLine());
-int[] numbers = new int[size];
-FillArrayRandomNumbers(numbers);
-Console.WriteLine("Вот наш массив: ");
-PrintArray(numbers);
-int count = 0;
+// Console.WriteLine("Введите размер массива");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int[] numbers = new int[size];
+// FillArrayRandomNumbers(numbers);
+// Console.WriteLine("Вот наш массив: ");
+// PrintArray(numbers);
+// int count = 0;
 
-for (int i = 0; i < numbers.Length; ++)
-if (numbers[z] % 2 == 0)
-count++;
+// for (int i = 0; i < numbers.Length; ++)
+// if (numbers[z] % 2 == 0)
+// count++;
 
-Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
+// Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
 
-void FillArrayRandomNumbers(int[] numbers)
-{
-    for(int i = 0; i < numbers.Length; i++)
-    {
-        numbers[i] = new Random().Next(100,1000);
-    }
-}
-void PrintArray(int[] numbers)
-{
-    Console.Write("[ ");
-    for(int i = 0; i < numbers.Length; i++)
-    {
-        Console.Write(numbers[i] + " ");
-    }
-    Console.Write("]");
-    Console.WriteLine();
-}
+// void FillArrayRandomNumbers(int[] numbers)
+// {
+//     for(int i = 0; i < numbers.Length; i++)
+//     {
+//         numbers[i] = new Random().Next(100,1000);
+//     }
+// }
+// void PrintArray(int[] numbers)
+// {
+//     Console.Write("[ ");
+//     for(int i = 0; i < numbers.Length; i++)
+//     {
+//         Console.Write(numbers[i] + " ");
+//     }
+//     Console.Write("]");
+//     Console.WriteLine();
+// }
 // Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов,
 //  стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
