5e325f502925155e3b9fa96cdb5604c458f90347
Elena <shigonoke@mail.ru>
2023-03-10 11:24:28 +0300

    Seminar9

---
+++ Seminar9/Program.cs
@@ -0,0 +1,63 @@
+﻿// Seminar9 Рекурсии
+// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+
+// void ShowNums(int num)
+// {
+//     if(num > 1) ShowNums(num - 1);
+//     Console.Write(num + " ");
+// }
+
+// ShowNums(5);
+
+// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+// int SumOfDigits(int num)
+// {
+//     if(num / 10 !=0)  return SumOfDigits(num / 10) + Math.Abs(num % 10);
+//     return Math.Abs(num);
+// }
+// Console.WriteLine(SumOfDigits(-1234));
+
+// Задачи в залах
+// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа
+//  в промежутке от M до N.
+// void ShowNums(int m, int n)
+// {
+//     Console.Write(m + " ");
+
+//     if (m > n) 
+//     ShowNums(m - 1, n);
+//     else if (m < n)
+//      ShowNums(m + 1, n);
+// }
+// ShowNums(8, 2);
+// Console.WriteLine();
+// ShowNums(2, 8);
+// Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B.
+
+// double Pow(double a, int b)
+// {
+//    if (b > 0)
+//     return  a* Pow(a, b-1);
+//     else if (b < 0)
+//         return (1 / a) * Pow(a, b + 1);
+//      else
+//         return 1 ;
+// }
+// Console.WriteLine(Pow(3,4));
+// Console.WriteLine(Pow(3,-2));
+// 2 вариант решения
+
+// double Power0fNum (double num, int pow)
+// {
+// if (pow == 0) return 1;
+// else
+// {
+// if (pow > 0)
+//     return Power0fNum(num, pow - 1) * num;
+// else
+//     return Power0fNum(num, pow + 1) / num;
+    
+// }
+// }
+// Console.WriteLine(Power0fNum(2,-4));
